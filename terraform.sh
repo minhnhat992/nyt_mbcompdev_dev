4295220fc62ac5192d321df75cd0aac8b62fed11
Nhat Bui <minhnhat992@gmail.com>
2021-12-29 14:03:23 -0500

    further terraform modification

--- terraform.sh
+++ terraform.sh
@@ -1,6 +1,10 @@
 cd terraform
-terraform init --upgrade \
+terraform init --upgrade  -reconfigure  \
 -var credentials_file="creds/nyt-mbcompdev-dev-5c351ded4d94.json" \
--backend-config=gcs_backend_circle.conf
+-backend-config=gcs_backend_local.conf
+
+terraform plan \
+-var terraform_credentials_file="creds/nyt-mbcompdev-dev-5c351ded4d94.json"
+
 
 terraform apply --auto-approve
