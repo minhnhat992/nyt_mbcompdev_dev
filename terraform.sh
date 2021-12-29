9f79e1809559b7f53b73fbd8f5164fa3fe1ac0c9
Nhat Bui <minhnhat992@gmail.com>
2021-12-29 13:55:01 -0500

    test gcs backend_config

--- terraform.sh
+++ terraform.sh
@@ -1,3 +1,6 @@
 cd terraform
-terraform init --upgrade
+terraform init --upgrade \
+-var credentials_file="creds/nyt-mbcompdev-dev-5c351ded4d94.json" \
+-backend-config=gcs_backend_circle.conf
+
 terraform apply --auto-approve
